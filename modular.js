2e8e241e7100816e63e4435c97a5424f592727fb
MiguelDotL <miguel.d.lozano@gmail.com>
2016-09-24 23:28:41 -0400

    Add minor styling fix

--- modular.js
+++ modular.js
@@ -1,43 +1,44 @@
 var people = {
     people: ['Martin', 'George', 'Kristin'],
     init: function() {
-      this.cacheDom();
-      this.bindEvents();
-      this.render();
+        this.cacheDom();
+        this.bindEvents();
+        this.render();
     },
     cacheDom: function() {
-      this.$el = $('#peopleModule');
-      this.$button = this.$el.find('button');
-      this.$input = this.$el.find('input');
-      this.$ul = this.$el.find('ul');
-      this.template = this.$el.find('#people-template').html();
+        this.$el = $('#peopleModule');
+        this.$button = this.$el.find('button');
+        this.$input = this.$el.find('input');
+        this.$ul = this.$el.find('ul');
+        this.template = this.$el.find('#people-template').html();
     },
     bindEvents: function() {
-      this.$button.on('click', this.addPerson.bind(this));
-      this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
+        this.$button.on('click', this.addPerson.bind(this));
+        this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
     },
     render: function() {
-      var data = {
-          people: this.people,
-      };
-      this.$ul.html(Mustache.render(this.template, data));
+       var data = {
+           people: this.people,
+       };
+       this.$ul.html(Mustache.render(this.template, data));
     },
-    addPerson: function(name || ) {
-      this.people.push(name || this.$input.val());
-      this.render();
-      this.$input.val('');
+    addPerson: function() {
+        this.people.push(this.$input.val());
+        this.render();
+        this.$input.val('');
     },
-    deletePerson:  function(event) {
-      var $remove = $(event.target).closest('li');
-      var i = this.$ul.find('li').index($remove);
+    deletePerson: function(event) {
+        var $remove = $(event.target).closest('li');
+        var i = this.$ul.find('li').index($remove);
 
-      this.people.splice(i, 1);
-      this.render();
+        this.people.splice(i, 1);
+        this.render();
     }
 
 };
 
 people.init();
+
 
 
 
