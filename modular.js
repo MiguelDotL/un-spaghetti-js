d061eb39d3703494f029b8173b5035b634076cf5
MiguelDotL <miguel.d.lozano@gmail.com>
2016-09-24 22:23:55 -0400

    Add deletePerson functionality

--- modular.js
+++ modular.js
@@ -15,6 +15,7 @@
       },
       bindEvents: function() {
         this.$button.on('click', this.addPerson.bind(this));
+        this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this));
       },
       render: function() {
         var data = {
@@ -25,7 +26,16 @@
       addPerson: function() {
         this.people.push(this.$input.val());
         this.render();
+        this.$input.val('');
+      },
+      deletePerson:  function(event) {
+        var $remove = $(event.target).closest('li');
+        var i = this.$ul.find('li').index($remove);
+
+        this.people.splice(i, 1);
+        this.render();
       }
+
   };
 
   people.init();
